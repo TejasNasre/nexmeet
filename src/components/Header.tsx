c48df9c9ae60ea69499a4244d9df78b406df2854
tejasnasre <tejasnasre120@gmail.com>
2024-07-30 20:53:13 +0530

    fix::ui

---
+++ src/components/Header.tsx
@@ -0,0 +1,53 @@
+"use client";
+import Link from "next/link";
+import { useState, useEffect } from "react";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { IoMdClose } from "react-icons/io";
+
+function Header() {
+  const [open, setOpen] = useState(false);
+
+  const toggle = () => {
+    setOpen(!open);
+    // console.log(open)
+  };
+
+  useEffect(() => {
+    document.body.style.overflow = open ? "hidden" : "auto";
+    return () => {
+      document.body.style.overflow = "auto"; // Cleanup on component unmount
+    };
+  }, [open]);
+  return (
+    <>
+      <div
+        className={`sticky top-0 z-50  
+          drop-shadow-xl transition-colors duration-500 ${
+            open
+              ? `flex flex-row justify-between items-center`
+              : `flex flex-row justify-between items-center `
+          } p-4 bg-[rgba(255, 255, 255, 0.01)] backdrop-blur-[10px]`}
+      >
+        <div className="text-4xl">NEXMEET</div>
+        <div className="block md:hidden" onClick={() => toggle()}>
+          {open ? <IoMdClose /> : <GiHamburgerMenu />}
+        </div>
+      </div>
+      <div
+        className={`${
+          open
+            ? `h-screen flex flex-col justify-center items-center gap-10`
+            : `hidden`
+        } md:flex md:flex-row md:justify-center md:items-center md:gap-4`}
+      >
+        <Link href="/">Home</Link>
+        <Link href="/">About Us</Link>
+        <Link href="/">Create Event</Link>
+        <Link href="/">SignIn</Link>
+        <Link href="/">Login</Link>
+      </div>
+    </>
+  );
+}
+
+export default Header;
