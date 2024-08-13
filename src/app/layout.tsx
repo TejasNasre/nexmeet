c2bab2d8772e60070d93eccc205db93318815f7c
Soham Khedkar <sohamunderscore99@gmail.com>
2024-08-13 16:43:50 +0530

    fix::ui

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Header from "@/components/Header";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,7 +17,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Header />
+        {/* <Header /> */}
         {children}
       </body>
     </html>
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,43 +1,13 @@
-import React, { useState } from "react";
+"use client"
+import React, { Suspense } from "react";
 import Spline from "@splinetool/react-spline/next";
-import AppleCardsCarouselDemo from "@/components/Carousel";
+
 const Hero: React.FC = () => {
-  const [isUpcoming, setIsUpcoming] = useState(true);
-  // const upcomingEventsData = [ /* Your upcoming events data here */ ];
-  // const pastEventsData = [ /* Your past events data here */ ];
   return (
     <div className="flex flex-col items-center">
-        <Spline
-          scene="https://prod.spline.design/6-yx5IQWbDEb-jBX/scene.splinecode"
-        />
-
-      <div className="flex space-x-4 mt-6">
-        <button
-          className={`px-4 py-2 rounded-l-lg ${
-            isUpcoming ? "bg-blue-500 text-white" : "bg-gray-200"
-          }`}
-          onClick={() => setIsUpcoming(true)}
-        >
-          Upcoming Events
-        </button>
-        <button
-          className={`px-4 py-2 rounded-r-lg ${
-            !isUpcoming ? "bg-blue-500 text-white" : "bg-gray-200"
-          }`}
-          onClick={() => setIsUpcoming(false)}
-        >
-          Past Events
-        </button>
-      </div>
-
-      {/* Placeholder for the card carousel */}
-      <div className="mt-8 w-full max-w-7xl">
-        {isUpcoming ? (
-          <AppleCardsCarouselDemo />
-        ) : (
-          <AppleCardsCarouselDemo />  
-        )}
-        </div>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Spline scene="https://prod.spline.design/w-PrOzkbP9FvMKE0/scene.splinecode" />
+      </Suspense>
     </div>
   );
 };
