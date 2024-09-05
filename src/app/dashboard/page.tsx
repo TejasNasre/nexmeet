72729fb9bd115487a4ff76eb4128ad7b9cd52f54
tejasnasre <tejasnasre120@gmail.com>
2024-09-05 21:25:02 +0530

    fix::dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -7,7 +7,6 @@ import { LogoutLink } from "@kinde-oss/kinde-auth-nextjs/components";
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-import { Badge } from "@/components/ui/badge";
 
 import {
   CalendarDays,
@@ -17,10 +16,13 @@ import {
   ChevronRight,
   ChevronLeft,
 } from "lucide-react";
-import { supabase } from "../../utils/supabase";
+
 import { useRouter } from "next/navigation";
 import Link from "next/link";
 import { mergeConfigs } from "tailwind-merge";
+import Organisedevent from "@/components/Organisedevent";
+import Participatedevent from "@/components/Participatedevent";
+import { supabase } from "@/utils/supabase";
 
 interface User {
   id: string;
@@ -31,7 +33,6 @@ interface User {
 }
 
 export default function Page() {
-  const router = useRouter();
   const [activeTab, setActiveTab] = useState("organized");
 
   const [organisedEvent, setOrganisedEvent] = useState<any[] | null>([]);
@@ -41,7 +42,6 @@ export default function Page() {
   useEffect(() => {
     userDetails()
       .then((res: any) => {
-        // console.log(res)
         setUser(res);
         setLoading(false);
       })
@@ -56,7 +56,7 @@ export default function Page() {
     const organizeEvents: any = async () => {
       let { data: organised_events, error } = await supabase
         .from("event_details")
-        .select("*,event_participants(*)")
+        .select("*")
         .eq("organizer_email", user?.email);
 
       if (error) {
@@ -66,22 +66,11 @@ export default function Page() {
         setLoading(true);
         setOrganisedEvent(organised_events);
         setLoading(false);
-        // console.log(organised_events);
       }
     };
 
     organizeEvents();
   }, [user]);
-
-  const participatedEvents = useMemo(() => {
-    return organisedEvent?.filter((event) =>
-      event.event_participants.filter(
-        (participant: any) => participant.email === user?.email
-      )
-    );
-  }, [organisedEvent, user?.email]);
-
-  const memoizedEvents = useMemo(() => organisedEvent, [organisedEvent]);
 
   return (
     <>
@@ -123,7 +112,7 @@ export default function Page() {
                 </CardHeader>
                 <CardContent>
                   <div className="text-2xl font-bold">
-                    {memoizedEvents?.length ? memoizedEvents.length : 0}
+                    {organisedEvent?.length ? organisedEvent.length : 0}
                   </div>
                   {/* <p className="text-xs text-purple-200">+2 this month</p> */}
                 </CardContent>
@@ -161,7 +150,7 @@ export default function Page() {
                 <Link href="/add-event">Organised Your Own Event</Link>
               </Button>
               <Button variant="outline" className="w-full">
-                <Link href="/events">View All Events</Link>
+                <Link href="/explore-events">View All Events</Link>
               </Button>
             </div>
 
@@ -193,65 +182,10 @@ export default function Page() {
               </CardHeader>
               <CardContent className={`${loading ? `h-auto` : `p-0`}`}>
                 <div className="divide-y">
-                  {memoizedEvents && memoizedEvents.length > 0 ? (
-                    loading ? (
-                      <div className="py-[10rem] flex flex-col justify-center items-center">
-                        <h1>Loading....</h1>
-                      </div>
-                    ) : (
-                      <>
-                        {memoizedEvents?.map((event) => (
-                          <div
-                            key={event.id}
-                            className="p-4 hover:bg-muted/50 transition-colors flex flex-col gap-2"
-                          >
-                            <div className="flex justify-between items-start mb-2">
-                              <div>
-                                <h3 className="font-semibold text-lg">
-                                  {activeTab === "organized"
-                                    ? `${event.event_title}`
-                                    : `${participatedEvents?.map(
-                                        (event) => event.event_title
-                                      )}`}
-                                </h3>
-                                <p className="text-sm text-muted-foreground">
-                                  {new Date(
-                                    event.event_startdate
-                                  ).toLocaleString(undefined, {
-                                    year: "numeric",
-                                    month: "short",
-                                    day: "numeric",
-                                  })}
-                                </p>
-                                <div className="flex items-center text-sm text-muted-foreground">
-                                  <MapPin size={16} className="mr-1" />
-                                  {event.event_location}
-                                </div>
-                              </div>
-                              <Badge variant="outline">Organizer</Badge>
-                            </div>
-
-                            <Button
-                              variant="outline"
-                              size="sm"
-                              className="w-[8rem] text-white"
-                            >
-                              <Link
-                                href={`/events/${event.id}`}
-                                className="flex flex-row justify-center items-center"
-                              >
-                                View Details{" "}
-                                <ChevronRight size={16} className="ml-1" />
-                              </Link>
-                            </Button>
-                          </div>
-                        ))}
-                      </>
-                    )
+                  {activeTab === "organized" ? (
+                    <Organisedevent user={user?.email} />
                   ) : (
-                    <div className="p-10 text-center text-muted-foreground">
-                      No events found
-                    </div>
+                    <Participatedevent user={user?.email} />
                   )}
                 </div>
               </CardContent>
