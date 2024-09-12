4c5e2d36a92d509e2fcdc576b0d745c32b174970
tejasnasre <tejasnasre120@gmail.com>
2024-09-12 17:17:36 +0530

    fix::metadata

--- src/app/(event)/explore-events/[eventsId]/page.tsx
+++ src/app/(event)/explore-events/[eventsId]/page.tsx
@@ -11,20 +11,21 @@ export async function generateMetadata({
 }): Promise<Metadata> {
   // Fetch event data
 
-  const { data: eventData, error } = await supabase
+  const { data: eventData, error }: any = await supabase
     .from("event_details")
-    .select("event_title, event_description, event_images")
-    .eq("id", params.eventsId)
-    .single();
+    .select("*,event_images(event_id,url)")
+    .eq("id", params.eventsId);
+
+  // console.log(eventData);
 
   if (!eventData) return {};
 
   const eventTitle = eventData.event_title || "Event";
   const eventDescription =
     eventData.event_description || "Check out this event!";
-  const eventImageUrl = eventData.event_images?.length
-    ? JSON.parse(eventData.event_images[0].url)[0]
-    : "https://jzhgfowuznosxtwzkbkx.supabase.co/storage/v1/object/public/event_image/_Black_And_Yellow_Modern_Event_Producer_Initial_Logo-removebg-preview.png"; // Default image
+  const eventImageUrl =
+    JSON.parse(eventData[0].event_images[0].url)[0] ||
+    "https://jzhgfowuznosxtwzkbkx.supabase.co/storage/v1/object/public/event_image/_Black_And_Yellow_Modern_Event_Producer_Initial_Logo-removebg-preview.png"; // Default image
 
   // Meta tags for WhatsApp will use OpenGraph tags
   return {
