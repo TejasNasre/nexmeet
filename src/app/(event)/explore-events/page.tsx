70d4acfd3007ccf629b81c3188593e92e6a7c851
tejasnasre <tejasnasre120@gmail.com>
2024-09-14 14:03:46 +0530

    fix:filtering

--- src/app/(event)/explore-events/page.tsx
+++ src/app/(event)/explore-events/page.tsx
@@ -8,15 +8,15 @@ import Loading from "../../../components/loading";
 import { CalendarIcon, MapPinIcon } from "lucide-react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
-import { json } from "stream/consumers";
 
 const Page: React.FC = () => {
   const [loading, setLoading] = useState(true);
   const [event, setEvent]: any = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
   const [itemsPerPage, setItemsPerPage] = useState(9);
-  const [numberOfLikes, setNumberOfLikes] = useState("0");
+  const [numberOfLikes, setNumberOfLikes] = useState("all");
   const [searchTerm, setSearchTerm] = useState("");
+  const [category, setCategory] = useState("");
   const [startDate, setStartDate] = useState<Date>();
   const [endDate, setEndDate] = useState<Date>();
 
@@ -29,33 +29,53 @@ const Page: React.FC = () => {
         console.error("Error fetching event details:", error);
       } else {
         setEvent(data);
-        // console.log(data);
-        // console.log(data[0].event_images[0].url);
       }
       setLoading(false);
     }
     getData();
   }, []);
 
-  const eventData = event.filter((event: any) => {
-    const date = new Date(event.event_startdate);
-    return (
-      parseInt(numberOfLikes) * 50 <= event.event_likes &&
-      (event.event_title.toLowerCase().includes(searchTerm.toLowerCase()) ||
-        event.event_category.includes(searchTerm.toLowerCase()) ||
-        event.event_location
-          .toLowerCase()
-          .includes(searchTerm.toLowerCase())) &&
-      (startDate == null || new Date(startDate) < date) &&
-      (endDate == null || date < new Date(endDate))
-    );
-  });
+  const sortEventsByLikes = (events: any[]) => {
+    if (numberOfLikes === "high") {
+      return events.sort((a, b) => b.event_likes - a.event_likes);
+    } else if (numberOfLikes === "low") {
+      return events.sort((a, b) => a.event_likes - b.event_likes);
+    }
+    return events;
+  };
+
+  const filteredAndSortedEvents = sortEventsByLikes(event).filter(
+    (event: any) => {
+      const date = new Date(event.event_startdate);
+
+      // Check if the event matches the category
+      const matchesCategory = category
+        ? event.event_category === category
+        : true;
+
+      // Check if the event matches the search term in title, category, or location
+      const matchesSearchTerm =
+        event.event_title.toLowerCase().includes(searchTerm.toLowerCase()) ||
+        event.event_location.toLowerCase().includes(searchTerm.toLowerCase());
+
+      // Check if the event is within the date range
+      const withinDateRange =
+        (startDate == null || new Date(startDate) < date) &&
+        (endDate == null || date < new Date(endDate));
+
+      // Return true if the event matches the category and matches the search term and is within the date range
+      return matchesCategory && matchesSearchTerm && withinDateRange;
+    }
+  );
 
   const indexOfLastItem = currentPage * itemsPerPage;
   const indexOfFirstItem = indexOfLastItem - itemsPerPage;
-  const currentItems = eventData.slice(indexOfFirstItem, indexOfLastItem);
+  const currentItems = filteredAndSortedEvents.slice(
+    indexOfFirstItem,
+    indexOfLastItem
+  );
 
-  const totalPages = Math.ceil(eventData.length / itemsPerPage);
+  const totalPages = Math.ceil(filteredAndSortedEvents.length / itemsPerPage);
 
   const handleStartDateChange = (date: Date) => {
     setStartDate(date); // Update the start date
@@ -77,17 +97,17 @@ const Page: React.FC = () => {
 
   return (
     <>
-      <div className="absolute top-0 w-full h-auto bg-black text-white py-[8rem] px-[2rem]">
+      <div className="absolute top-0 w-full h-auto bg-black text-white py-[8rem] px-4">
         <div className="text-center my-10 text-4xl font-bold">
           Explore Events
         </div>
         <div className="w-full my-[3rem] flex flex-col gap-4 justify-end">
           <div>
-            <label className="input input-bordered flex items-center gap-2 bg-black border border-white w-full">
+            <label className="flex items-center gap-2 bg-black border border-white w-full rounded-md px-4">
               <input
                 type="text"
-                className="text-white grow w-full"
-                placeholder="Search Name, Category or Location"
+                className="bg-black text-white w-full p-2 rounded-md border-0 outline-none"
+                placeholder="Search Name Or Location"
                 value={searchTerm}
                 onChange={(e) => setSearchTerm(e.target.value)}
               />
@@ -105,108 +125,120 @@ const Page: React.FC = () => {
               </svg>
             </label>
           </div>
-          <div className="w-full flex justify-end items-center gap-4">
-            <DatePicker
-              className="date-picker w-full border border-white p-2 rounded-md bg-black text-white h-12"
-              selected={startDate}
-              onChange={(date) => date && handleStartDateChange(date)}
-              placeholderText="Start Date"
-            />
-            <DatePicker
-              className="date-picker w-full border border-white p-2 rounded-md bg-black text-white h-12"
-              selected={endDate}
-              onChange={(date) => date && handleEndDateChange(date)}
-              placeholderText="End Date"
-              minDate={startDate} // Prevent end date from being before start date
-            />
-            <select
-              className="w-[8rem] bg-black text-start text-white border border-white outline-black rounded-md"
-              value={numberOfLikes}
-              onChange={(e) => setNumberOfLikes(e.target.value)}
-            >
-              <option value={0}>Likes</option>
-              <option value={1}>50+</option>
-              <option value={2}>100+</option>
-              <option value={3}>150+</option>
-            </select>
+
+          <div className="w-full flex justify-end items-center flex-col md:flex-row gap-4">
+            <div className="flex flex-row justify-center gap-4">
+              <DatePicker
+                className="date-picker w-[8rem] border border-white p-2 rounded-md bg-black text-white"
+                selected={startDate}
+                onChange={(date) => date && handleStartDateChange(date)}
+                placeholderText="Start Date"
+              />
+              <DatePicker
+                className="date-picker w-[8rem] border border-white p-2 rounded-md bg-black text-white"
+                selected={endDate}
+                onChange={(date) => date && handleEndDateChange(date)}
+                placeholderText="End Date"
+                minDate={startDate} // Prevent end date from being before start date
+              />
+            </div>{" "}
+            <div className="flex flex-row justify-center gap-4">
+              <select
+                className="w-[8rem] border border-white p-2 rounded-md bg-black text-white"
+                value={category}
+                onChange={(e) => setCategory(e.target.value)}
+              >
+                <option value="">Category</option>
+                <option value="technical">Technical</option>
+                <option value="sports">Sports</option>
+                <option value="cultural">Cultural</option>
+                <option value="meetup">Meetup</option>
+                <option value="conference">Conference</option>
+              </select>
+              <select
+                className="w-[10rem] border border-white p-2 rounded-md bg-black text-white"
+                value={numberOfLikes}
+                onChange={(e) => setNumberOfLikes(e.target.value)}
+              >
+                <option value="all">Likes</option>
+                <option value="high">Highest Like</option>
+                <option value="low">Lowest Likes</option>
+              </select>
+            </div>
           </div>
         </div>
         {loading ? (
           <Loading />
         ) : (
-          <>
-            <div className="w-full flex flex-wrap gap-5 justify-evenly py-[4rem]">
-              {eventData.length > 0 ? (
-                eventData.map((event: any) => (
-                  <div
-                    className="cursor-pointer w-[350px] mx-auto bg-black text-white rounded-xl shadow-md overflow-hidden transition duration-300 ease-in-out transform hover:scale-105"
-                    key={event.id}
-                  >
-                    <div className="relative h-64">
-                      <Image
-                        width="500"
-                        height="500"
-                        src={JSON.parse(event.event_images[0]?.url)[0]}
-                        alt={event.event_title}
-                        className="w-full h-full object-cover"
-                      />
-                      <div className="absolute bottom-0 left-0 right-0 bg-gradient-to-t from-black to-transparent p-4">
-                        <h2 className="text-lg font-bold leading-tight text-white">
-                          {event.event_title}
-                        </h2>
-                      </div>
-                    </div>
-                    <div className="p-4 text-white">
-                      <div className="flex justify-between items-start mb-2">
-                        <span className="px-2 py-1 text-xs font-semibold rounded-full bg-purple-100 text-purple-800">
-                          {event.event_category}
-                        </span>
-                        <span className="text-sm font-semibold">
-                          ${event.event_price}
-                        </span>
-                      </div>
-                      <p className="text-xs mb-3 line-clamp-2">
-                        {event.event_description}
-                      </p>
-                      <div className="space-y-2">
-                        <div className="flex items-center space-x-2 text-xs">
-                          <CalendarIcon className="h-3 w-3" />
-                          <span>
-                            {" "}
-                            {new Date(event.event_startdate).toLocaleString(
-                              undefined,
-                              {
-                                year: "numeric",
-                                month: "short",
-                                day: "numeric",
-                              }
-                            )}
-                          </span>
-                        </div>
-                        <div className="flex items-center space-x-2 text-xs">
-                          <MapPinIcon className="h-3 w-3" />
-                          <span className="truncate">
-                            {event.event_location}
-                          </span>
-                        </div>
-                      </div>
-                    </div>
-                    <div className="px-4 pb-4">
-                      <Link href={`/explore-events/${event.id}`}>
-                        <button className="w-full bg-black border text-white text-sm font-semibold py-2 px-4 rounded-lg transition duration-300 ease-in-out">
-                          View Details
-                        </button>
-                      </Link>
+          <div className="w-full flex flex-wrap gap-5 justify-evenly py-[4rem]">
+            {currentItems.length > 0 ? (
+              currentItems.map((event: any) => (
+                <div
+                  className="cursor-pointer w-[350px] mx-auto bg-black text-white rounded-xl shadow-md overflow-hidden transition duration-300 ease-in-out transform hover:scale-105"
+                  key={event.id}
+                >
+                  <div className="relative h-64">
+                    <Image
+                      width="500"
+                      height="500"
+                      src={JSON.parse(event.event_images[0]?.url)[0]}
+                      alt={event.event_title}
+                      className="w-full h-full object-cover"
+                    />
+                    <div className="absolute bottom-0 left-0 right-0 bg-gradient-to-t from-black to-transparent p-4">
+                      <h2 className="text-lg font-bold leading-tight text-white">
+                        {event.event_title}
+                      </h2>
                     </div>
                   </div>
-                ))
-              ) : (
-                <div className="h-screen flex flex-col justify-center items-center text-3xl font-bold">
-                  Event Not Found
+                  <div className="p-4 text-white">
+                    <div className="flex justify-between items-start mb-2">
+                      <span className="px-2 py-1 text-xs font-semibold rounded-full bg-purple-100 text-purple-800">
+                        {event.event_category}
+                      </span>
+                      <span className="text-sm font-semibold">
+                        ${event.event_price}
+                      </span>
+                    </div>
+                    <p className="text-xs mb-3 line-clamp-2">
+                      {event.event_description}
+                    </p>
+                    <div className="space-y-2">
+                      <div className="flex items-center space-x-2 text-xs">
+                        <CalendarIcon className="h-3 w-3" />
+                        <span>
+                          {" "}
+                          {new Date(event.event_startdate).toLocaleString(
+                            undefined,
+                            {
+                              year: "numeric",
+                              month: "short",
+                              day: "numeric",
+                            }
+                          )}
+                        </span>
+                      </div>
+                      <div className="flex items-center space-x-2 text-xs">
+                        <MapPinIcon className="h-3 w-3" />
+                        <span className="truncate">{event.event_location}</span>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="px-4 pb-4">
+                    <Link href={`/explore-events/${event.id}`}>
+                      <button className="w-full bg-black border text-white text-sm font-semibold py-2 px-4 rounded-lg transition duration-300 ease-in-out">
+                        View Details
+                      </button>
+                    </Link>
+                  </div>
                 </div>
-              )}
-            </div>
-          </>
+              ))
+            ) : (
+              <div className="h-screen flex flex-col justify-center items-center text-3xl font-bold">
+                Event Not Found
+              </div>
+            )}
+          </div>
         )}
 
         <div className="text-center mt-[3rem]">
