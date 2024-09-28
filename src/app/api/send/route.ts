1bf45db4da67c1c01004d6f6bab7959c62cd7e13
tejasnasre <tejasnasre120@gmail.com>
2024-09-28 17:07:58 +0530

    add::pre-commit-hooks and resend setup

---
+++ src/app/api/send/route.ts
@@ -0,0 +1,23 @@
+import { RegisterEmail } from '../../../components/email-templates/register-email';
+import { Resend } from 'resend';
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+export async function POST() {
+  try {
+    const { data, error } = await resend.emails.send({
+      from: 'Acme <onboarding@resend.dev>',
+      to: ['nexmeetup@gmail.com'],
+      subject: 'Hello world',
+      react: RegisterEmail({ firstName: 'John' }),
+    });
+
+    if (error) {
+      return Response.json({ error }, { status: 500 });
+    }
+
+    return Response.json(data);
+  } catch (error) {
+    return Response.json({ error }, { status: 500 });
+  }
+}
