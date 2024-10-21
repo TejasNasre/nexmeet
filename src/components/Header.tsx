ab23a669606896720b33915c8b69be8f802af3ac
Mehul <mehulprajapati2802@gmail.com>
2024-10-20 20:44:12 -0400

    Added dropdown options and remove profile details from dashboard

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -11,9 +11,13 @@ import Image from "next/image";
 import { useKindeBrowserClient } from "@kinde-oss/kinde-auth-nextjs";
 import { BsBrightnessLow } from "react-icons/bs";
 import { IoMoon } from "react-icons/io5";
+import { LogoutLink } from "@kinde-oss/kinde-auth-nextjs/components";
 
 interface User {
   picture: string;
+  given_name: string;
+  family_name: string;
+  email: string;
 }
 
 function Header() {
@@ -21,6 +25,7 @@ function Header() {
 
   const [user, setUser] = useState<User | null>(null);
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false); // State for dropdown
   const [isDarkMode, setIsDarkMode] = useState(false);
   const router = useRouter();
 
@@ -48,6 +53,10 @@ function Header() {
   const toggleTheme = () => {
     setIsDarkMode(!isDarkMode);
     document.body.classList.toggle("white"); // Ensure dark-mode CSS is applied
+  };
+
+  const toggleDropdown = () => {
+    setIsDropdownOpen(!isDropdownOpen);
   };
 
   return (
@@ -132,94 +141,62 @@ function Header() {
     return (
       <>
         <div className={`flex justify-center items-center gap-6 ${isMenuOpen ? `flex-col` : `flex-row`}`}>
-          <Link
-            href="/"
-            onClick={() => handleNavigation("/")}
-            className="mono border border-white rounded-md px-4 py-2 hover:bg-gray-700"
-          >
-            Home
-          </Link>
-          <Link
-            href="/explore-events"
-            onClick={() => handleNavigation("/explore-events")}
-            className="mono border border-white rounded-md px-4 py-2 hover:bg-gray-700"
-          >
-            Explore Events
-          </Link>
+          <Link href="/" onClick={() => handleNavigation("/")} className="mono border border-white rounded-md px-4 py-2 hover:bg-gray-700">Home</Link>
+          <Link href="/explore-events" onClick={() => handleNavigation("/explore-events")} className="mono border border-white rounded-md px-4 py-2 hover:bg-gray-700">Explore Events</Link>
           {isAuthenticated && (
-            <Link
-              href="/explore-event-space"
-              onClick={() => handleNavigation("/explore-event-space")}
-              className="mono border border-white rounded-md px-4 py-2 hover:bg-gray-700"
-            >
-              Explore Event Spaces
-            </Link>
+            <Link href="/explore-event-space" onClick={() => handleNavigation("/explore-event-space")} className="mono border border-white rounded-md px-4 py-2 hover:bg-gray-700">Explore Event Spaces</Link>
           )}
-          <Link
-            href="/about"
-            onClick={() => handleNavigation("/about")}
-            className="mono border border-white rounded-md px-4 py-2 hover:bg-gray-700"
-          >
-            About Us
-          </Link>
-          <Link
-            href="/contact"
-            onClick={() => handleNavigation("/contact")}
-            className="mono border border-white rounded-md px-4 py-2 hover:bg-gray-700"
-          >
-            Contact
-          </Link>
-          <Link
-            href="/contributors"
-            onClick={() => handleNavigation("/contributors")}
-            className="mono border border-white rounded-md px-4 py-2 hover:bg-gray-700"
-          >
-            Contributors
-          </Link>
+          <Link href="/about" onClick={() => handleNavigation("/about")} className="mono border border-white rounded-md px-4 py-2 hover:bg-gray-700">About Us</Link>
+          <Link href="/contact" onClick={() => handleNavigation("/contact")} className="mono border border-white rounded-md px-4 py-2 hover:bg-gray-700">Contact</Link>
+          <Link href="/contributors" onClick={() => handleNavigation("/contributors")} className="mono border border-white rounded-md px-4 py-2 hover:bg-gray-700">Contributors</Link>
         </div>
         {isAuthenticated ? (
           <>
-            <Link
-              onClick={() => handleNavigation("/dashboard")}
-              href="/dashboard"
-              className="mono justify-center items-center flex hover:text-gray-300"
-            >
-              <Image
-                src={user?.picture || "/profile.jpg"}
-                alt="Profile"
-                width={56}
-                height={56}
-                className="rounded-full size-10 border-2 border-white"
-              />
-            </Link>
-            <button onClick={toggleTheme} className="bg-black p-2 rounded-md">
-              {isDarkMode ? (
-                <BsBrightnessLow size={24} />
-              ) : (
-                <IoMoon size={24} />
+            <div className="relative">
+              <Link onClick={() => toggleDropdown()} href="" className="mono justify-center items-center flex hover:text-gray-300">
+                <Image
+                  src={user?.picture || "/profile.jpg"}
+                  alt="Profile"
+                  width={56}
+                  height={56}
+                  className="rounded-full size-10 border-2 border-white"
+                />
+              </Link>
+              {isDropdownOpen && (
+                <div className="absolute right-0 mt-2 w-48 bg-black rounded-md shadow-lg z-20">
+                  <div className="py-1 px-2 text-white-700">
+                    <p className="font-bold">{user?.given_name} {user?.family_name}</p>
+                    <p className="text-sm">{user?.email}</p>
+                  </div>
+                  <div className="flex flex-col justify-center">
+                    <Link onClick={() => handleNavigation("/dashboard")} href="/dashboard" className="mono rounded-md px-2 py-2 hover:bg-gray-700">
+                        Dashboard
+                    </Link>
+                  </div>
+                  <div className="flex flex-col justify-center">
+                    <LogoutLink
+                      className="mono rounded-md px-2 py-2 hover:bg-gray-700"
+                      postLogoutRedirectURL="/"
+                    >Log out
+                    </LogoutLink>
+                  </div>
+                </div>
               )}
+            </div>
+            <button onClick={toggleTheme} className="bg-black p-2 rounded-md">
+              {isDarkMode ? <BsBrightnessLow size={24} /> : <IoMoon size={24} />}
             </button>
           </>
         ) : (
           <>
-            <LoginLink
-              postLoginRedirectURL="/dashboard"
-              className="mono transition ease-in-out delay-100 hover:scale-105 border-white border-double border-2 hover:border-white hover:shadow-[5px_5px_0px_0px_rgb(255,255,255)] rounded-md px-4 py-1"
-            >
+            <LoginLink postLoginRedirectURL="/dashboard" className="mono transition ease-in-out delay-100 hover:scale-105 border-white border-double border-2 hover:border-white hover:shadow-[5px_5px_0px_0px_rgb(255,255,255)] rounded-md px-4 py-1">
               Sign in
             </LoginLink>
-            <RegisterLink
-              postLoginRedirectURL="/dashboard"
-              className="mono transition ease-in-out delay-100 hover:scale-105 border-white border-double border-2 hover:border-white hover:shadow-[5px_5px_0px_0px_rgb(255,255,255)] rounded-md px-4 py-1"
-            >
+            <RegisterLink postLoginRedirectURL="/dashboard" className="mono transition ease-in-out delay-100 hover:scale-105 border-white border-double border-2 hover:border-white hover:shadow-[5px_5px_0px_0px_rgb(255,255,255)] rounded-md px-4 py-1">
               Sign up
             </RegisterLink>
             <button onClick={toggleTheme} className="bg-black p-2 rounded-md">
-              {isDarkMode ? (
-                <BsBrightnessLow size={24} />
-              ) : (
-                <IoMoon size={24} />
-              )}
+              {isDarkMode ? <BsBrightnessLow size={24} /> : <IoMoon size={24} />}
             </button>
           </>
         )}
