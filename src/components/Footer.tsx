dfcbb0a01ef6577e80d53dadc47ce0d4c8e9d564
Shariq <shariqsd2003@gmail.com>
2024-10-29 23:23:25 +0530

    Added Multi Lang

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,17 +1,42 @@
+"use client";
 import Link from "next/link";
 import { IoLogoTwitter, IoLogoGithub, IoLogoLinkedin } from "react-icons/io5";
-import React from "react";
+import React, { useEffect, useState, useRef } from "react";
 import { FaXTwitter } from "react-icons/fa6";
 import Image from "next/image";
 
 const Footer: React.FC = () => {
+  const [currentYear, setCurrentYear] = useState<number>(
+    new Date().getFullYear()
+  );
+  const isScriptAdded = useRef(false); 
+  useEffect(() => {
+    if (typeof window !== "undefined" && !isScriptAdded.current) {
+      window.gtranslateSettings = {
+        default_language: "en",
+        detect_browser_language: true,
+        wrapper_selector: ".gtranslate_wrapper",
+        font_size: 100,
+      };
+
+      // Dynamically add the script only once
+      const script = document.createElement("script");
+      script.src = "https://cdn.gtranslate.net/widgets/latest/popup.js";
+      script.defer = true;
+      document.body.appendChild(script);
+
+      isScriptAdded.current = true; 
+    }
+  }, []);
+
   return (
     <footer className="w-full bg-black text-white py-10 px-4 md:px-10 z-[999]">
       <div className="container mx-auto flex flex-col gap-8 md:flex-row justify-between items-center">
         <div className="text-center md:text-left">
-        <Link
+          <Link
             href={"/"}
-            className=" transition duration-300 ease-in-out transform hover:scale-105">
+            className="transition duration-300 ease-in-out transform hover:scale-105"
+          >
             <Image
               src={"/nexmeet.png"}
               width={500}
@@ -45,6 +70,10 @@ const Footer: React.FC = () => {
           >
             <IoLogoLinkedin className="h-6 w-6 text-white" />
           </Link>
+          <div
+            className="gtranslate_wrapper"
+            style={{ position: "absolute", bottom: "30px", left: "30px" }}
+          ></div>
         </div>
 
         <div className="flex flex-col md:flex-row gap-6 text-center">
@@ -64,7 +93,7 @@ const Footer: React.FC = () => {
       </div>
 
       <div className="border-t border-gray-700 mt-10 pt-6 text-center text-gray-500">
-        <p>&copy; {new Date().getFullYear()} Nexmeet. All Rights Reserved.</p>
+        <p>&copy; {currentYear} Nexmeet. All Rights Reserved.</p>
       </div>
     </footer>
   );
