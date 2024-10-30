583d961cd91c2ac127dcb7ff9e84e0f9105c357f
jassbawa <jasspreetsbawa@gmail.com>
2024-10-30 18:08:41 +0530

    feat: added google maps integration

---
+++ src/components/form-fields/LocationAutocomplete.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Autocomplete from "react-google-autocomplete";
+
+interface Props {
+    onSelect: (e: string) => void;
+    placeholder?: string;
+}
+
+const LocationAutocomplete: React.FC<Props> = ({ onSelect, placeholder }) => (
+  <Autocomplete
+    placeholder={placeholder ?? "Enter Event Location"}
+    apiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}
+    onPlaceSelected={({ formatted_address }) => onSelect(formatted_address ?? "")}
+    className="w-full p-2 text-white bg-black border border-white rounded-md"
+  />
+);
+
+export default LocationAutocomplete;
