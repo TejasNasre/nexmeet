b07af0f7a3d352aa9c0189e8e5bac7ea56dbca6a
jassbawa <jasspreetsbawa@gmail.com>
2024-10-31 13:56:29 +0530

    fix: autocomplete comple location results

--- src/components/form-fields/LocationAutocomplete.tsx
+++ src/components/form-fields/LocationAutocomplete.tsx
@@ -10,6 +10,10 @@ const LocationAutocomplete: React.FC<Props> = ({ onSelect, placeholder }) => (
   <Autocomplete
     placeholder={placeholder ?? "Enter Event Location"}
     apiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}
+    options={{
+      types: ["establishment"],
+      componentRestrictions: { country: "in" }, // Adjust country as needed
+    }}
     onPlaceSelected={({ formatted_address }) => onSelect(formatted_address ?? "")}
     className="w-full p-2 text-white bg-black border border-white rounded-md"
   />
